10e888525617648e90eb0c3a246c48d0c1a80812
Stefano Coretta <stefanoc.social@me.com>
2018-01-18 11:29:35 +0000

    add backup script

---
+++ data-backup.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Push test
+
+rsync -anv --no-whole-file --delete --exclude '*.DS_Store' \
+    ~/GitHub/glottaling/data/ \
+    /Volumes/Multimedia/ling-data-backup/glottaling/data/
+
+## Push run
+
+rsync -avz --no-whole-file --delete --exclude '*.DS_Store' \
+    ~/GitHub/glottaling/data/ \
+    /Volumes/Multimedia/ling-data-backup/glottaling/data/
+
+## Pull test
+
+rsync -anv --no-whole-file --delete --exclude '*.DS_Store' \
+    /Volumes/Multimedia/ling-data-backup/glottaling/data/
+    ~/GitHub/glottaling/data/ \
+
+## Pull run
+
+rsync -avz --no-whole-file --delete --exclude '*.DS_Store' \
+    /Volumes/Multimedia/ling-data-backup/glottaling/data/
+    ~/GitHub/glottaling/data/ \
